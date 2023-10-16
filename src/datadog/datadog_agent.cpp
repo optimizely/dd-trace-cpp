d200e0e8539e3d49b4b929bbbeea1a755a13afff
David Goffredo <david.goffredo@datadoghq.com>
2023-10-16 17:01:04 +0200

    let's extend the deadline

--- src/datadog/datadog_agent.cpp
+++ src/datadog/datadog_agent.cpp
@@ -139,7 +139,8 @@ DatadogAgent::DatadogAgent(const FinalizedDatadogAgentConfig& config,
 }
 
 DatadogAgent::~DatadogAgent() {
-  const auto deadline = clock_().tick + std::chrono::seconds(2);
+  // const auto deadline = clock_().tick + std::chrono::seconds(2);
+  const auto deadline = clock_().tick + std::chrono::seconds(30);
   cancel_scheduled_flush_();
   flush();
   http_client_->drain(deadline);
